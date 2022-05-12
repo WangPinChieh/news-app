638242f9773e73fd6f0502b467716dd46a7227e4
Jay Wang <jay.wang@hp.com>
2022-05-12 08:18:56 +0800

    feat: add userSlice

--- src/App.js
+++ src/App.js
@@ -1,21 +1,39 @@
 import "./App.css";
-import React, { useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import {
-  increment,
-  decrement,
-  incrementAmount,
-  fetchCountAsync,
-} from "./feature/counter/counterSlice";
-import {Counter} from "./feature/counter/index";
+import React, { useEffect } from "react";
+import { Counter } from "./feature/counter/index";
+import { useSelector, useDispatch } from "react-redux";
+import { setUsers } from "./feature/user/userSlice";
 
 function App() {
-  const [amount, setAmount] = useState(0);
-  const { count, loadingStatus } = useSelector((state) => state.counter);
   const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(
+      setUsers([
+        { name: "jay", age: 20 },
+        { name: "yvonne", age: 21 },
+        { name: "ben", age: 18 },
+      ])
+    );
+  }, []);
+  const { users } = useSelector((state) => state.users);
   return (
     <div className="App">
-    <Counter />
+      <Counter />
+      Length: {users.length}
+      <table>
+        <tr>
+          <th>Name</th>
+          <th>Age</th>
+        </tr>
+        {users.map((user) => {
+          return (
+            <tr>
+              <td>{user.name}</td>
+              <td>{user.age}</td>
+            </tr>
+          );
+        })}
+      </table>
     </div>
   );
 }
