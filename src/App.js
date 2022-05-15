52449be619b318a6598f91f8a376018d5fb1e6fd
Jay Wang <jay.wang@hp.com>
2022-05-15 22:16:22 +0800

    feat: use react-table

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import React, { useEffect } from "react";
 import { Counter } from "./feature/counter/index";
 import { useSelector, useDispatch } from "react-redux";
 import { setUsers } from "./feature/user/userSlice";
-import { UserTable } from "./feature/user/index";
+// import { UserTable } from "./feature/user/index";
+import { Table } from "./feature/custom-react-table/index";
 import { v4 as uuidv4 } from "uuid";
 
 function App() {
@@ -11,18 +12,37 @@ function App() {
   useEffect(() => {
     dispatch(
       setUsers([
-        { name: "jay", age: 20 },
-        { name: "yvonne", age: 21 },
-        { name: "ben", age: 18 },
+        { firstName: "jay", lastName: "wang", age: 20 },
+        { firstName: "yvonne", lastName: "shao", age: 21 },
+        { firstName: "ben", lastName: "chen", age: 18 },
       ])
     );
   }, []);
   const { users } = useSelector((state) => state.users);
+  const columns = React.useMemo(
+    () => [
+      {
+        Header: "First Name",
+        accessor: "firstName",
+      },
+      {
+        Header: "Last Name",
+        accessor: "lastName",
+      },
+      {
+        Header: "Age",
+        accessor: "age",
+      },
+    ],
+    []
+  );
+
   return (
     <div className="App">
       <Counter />
       Length: {users.length}
-      <UserTable columnNames={["name", "age"]} data={users} />
+      {/* <UserTable columnNames={["name", "age"]} data={users} /> */}
+      <Table columns={columns} data={users} />
     </div>
   );
 }
